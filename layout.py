72847144de529f73286a84a90e136102f0acafeb
Laureano Hess <laureanohess0@gmail.com>
2022-07-13 11:58:58 -0300

    :sparkles: Implementado algoritmo con la versión con temperatura.

--- layout.py
+++ layout.py
@@ -3,11 +3,12 @@ import numpy as np
 
 
 class LayoutGraph:
-    def __init__(self, grafo, iters, refresh, c1, c2, verbose=False):
+    def __init__(self, grafo, iters, temp, refresh, c1, c2, verbose=False, width=200, height=200):
         """
         Parámetros:
         grafo: grafo en formato lista
         iters: cantidad de iteraciones a realizar
+        temp: temperatura inicial
         refresh: cada cuántas iteraciones graficar. Si su valor es cero, entonces debe graficarse solo al final.
         c1: constante de repulsión
         c2: constante de atracción
@@ -18,21 +19,27 @@ class LayoutGraph:
         self.grafo = grafo
         self.cant_vert = len(grafo[0])
         
-
         self.vert2idx = {grafo[0][i]:i for i in range(self.cant_vert)}
-        print(self.vert2idx)
+
         # Inicializo estado
         self.posiciones = np.empty((self.cant_vert, 2)) 
         self.fuerzas = np.empty((self.cant_vert, 2))
 
         # Guardo opciones
         self.iters = iters
+        self.temp = temp
         self.verbose = verbose
         self.refresh = refresh
-        self.c1 = c1
-        self.c2 = c2
+        self.c1 = c1    # Constante de atracción
+        self.c2 = c2    # Constante de repulsión
+        self.c3 = 0.95  # Constante de cambio de temperatura
         
-        self.plt_fig, self.plt_ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+        self.width = width
+        self.height = height
+
+        plt.xlim(0, width)
+        plt.ylim(0, height)
+
         self._init_positions()
 
     def layout(self):
@@ -45,17 +52,18 @@ class LayoutGraph:
             self._draw()
 
     def _step(self):
-        #_initialize_temperature()
         self._initialize_accumulators()
         self._compute_attraction_forces()
         self._compute_repulsion_forces()
-        #self._compute_gravity_forces()
+        self._compute_gravity_forces()
         self._update_positions()
-        #_update_temperature()
-        pass
+        self._update_temperature()
     
     def _idx(self, v):
         return self.vert2idx[v]
+
+    def _initialize_temperature(self):
+        pass
 
     def _init_positions(self):
         self.posiciones = np.random.rand(self.cant_vert, 2)
@@ -90,13 +98,35 @@ class LayoutGraph:
                     self.fuerzas[self._idx(v_i)] += f_xy
                     self.fuerzas[self._idx(v_j)] -= f_xy
     
-    def _update_positions(self):
-        self.posiciones += self.fuerzas
+    def _compute_gravity_forces(self):
+        for v in self.grafo[0]:
+            center = (self.height // 2, self.width // 2)
+            pos = self._get_vertex_pos(v)
 
-    def _draw(self):
+            diff = center - pos
+            dist = np.linalg.norm(diff)
+
+            mod_f = self.f_gravity(dist, self.c1)
+            f_xy = (mod_f / dist) * diff
+
+            self.fuerzas[self._idx(v)] += f_xy
+
+    def _update_positions(self):
+        for v in self.grafo[0]:
+            vec_f = self.fuerzas[self._idx(v)]
+            norm_f = np.linalg.norm(vec_f)
+            if norm_f > self.temp:
+                vec_f = (self.temp / norm_f) * vec_f
+                #self._fuerzas[self._idx(v)] = vec_f
+            self.posiciones[self._idx(v)] += vec_f
+
+    def _draw(self, wait=0.2):
+        plt.pause(wait)
+        plt.cla()
+
         for v in self.grafo[0]:
             x, y = self._get_vertex_pos(v) 
-            plt.scatter(x, y, 1000)
+            plt.scatter(x, y, 1000, c="lightblue")
 
         for (orig, dest) in self.grafo[1]:
             p_orig =  self._get_vertex_pos(orig)
@@ -106,7 +136,10 @@ class LayoutGraph:
             y = [p_orig[1], p_dest[1]]
             plt.plot(x, y, color="gray")
         
-        plt.show()
+        plt.draw()
+
+    def _update_temperature(self):
+        self.temp *= self.c3
 
     @staticmethod
     def f_attraction(d, k):
@@ -115,4 +148,8 @@ class LayoutGraph:
     @staticmethod
     def f_repulsion(d, k):
         return (k**2) / d
+    
+    @classmethod
+    def f_gravity(cls, d, k):
+        return 0.1 * cls.f_attraction(d, k)
 
